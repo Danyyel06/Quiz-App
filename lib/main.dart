32ebbfe85df6aad33a5d77d36b04de6a5ec070d5
Danyyel06 <disokpehi58@gmail.com>
2025-07-08 20:07:21 +0100

    Changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black87,
-        body: Center(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'In London, UK, if you happen to die in the house of Parliament, you are techically entitled to a State Funeral, because the building is considered to be a sacred building.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 19, color: Colors.white),
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    minimumSize: const Size(550, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ), // width, height
-                  ),
-                  child: Text("False", style: TextStyle(color: Colors.white)),
-                ),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.greenAccent,
-
-                    minimumSize: const Size(550, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ), // width, height
-                  ),
-                  child: Text("False", style: TextStyle(color: Colors.white)),
-                ),
-              ],
-            ),
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizApp(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
+
+  @override
+  State<QuizApp> createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Center(
+            child: Text(
+              "This is where the question will go",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25.0, color: Colors.white),
+            ),
+          ),
+        ),
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              onPressed: () {},
+              child: Text("True", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ),
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              onPressed: () {},
+              child: Text("True", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
